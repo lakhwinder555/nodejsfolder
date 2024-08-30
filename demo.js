c7bacca2fd31a0b9686cbdec1354ef3f5af84f39
lakhwinder555 <lakhwindersinghwins@gmail.com>
2024-08-30 13:18:20 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 var http = require('http');
  
 http.createServer(function (req, res) {
-    res.write("Hello from Node.js server123, Hello again");
+    res.write("Hello from Dits");
     res.end();
 }).listen(4000, function() {
     console.log("Server is listening on port 4000");
